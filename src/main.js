ae457ff08b4aabcc303a6cae2b18af54d136a982
w1y0n0 <wiyono30@gmail.com>
2026-06-03 13:19:46 +0700

    Membuat Helpers Menggunakan Mixins
    - Membuat Mixins
    - Register Global Mixins

--- src/main.js
+++ src/main.js
@@ -12,10 +12,18 @@ import "vue-toastification/dist/index.css";
 */
 import './style.css'
 
+/**
+ * Mixins
+ */
+import mixins from './mixins';
+
 // create App Vue
 const app = createApp(App)
 
 // gunakan "Toast" di Vue Js dengan plugin "use"
 app.use(Toast)
+
+// gunakan "Mixins" di Vue Js dengan plugin "use"
+app.mixin(mixins)
 
 app.mount('#app')
