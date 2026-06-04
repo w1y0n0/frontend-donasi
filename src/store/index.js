380b35e970fe38fc98b16520342bc3cb4a831aa3
w1y0n0 <wiyono30@gmail.com>
2026-06-04 09:16:17 +0700

    Konfigurasi Module Donation Vuex
    - Membuat Module Donation Vuex
    - Import Module Donation di Store Vuex

--- src/store/index.js
+++ src/store/index.js
@@ -4,11 +4,15 @@ import { createStore } from 'vuex'
 //import module auth
 import auth from './module/auth'
 
+//import module donation
+import donation from './module/donation'
+
 //create store vuex
 export default createStore({
 
     modules: {
         auth,    // <-- module auth
+        donation,   // <-- module donation
     }
 
 })
