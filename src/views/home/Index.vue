1e64fa7dd2d0dce5c9fc8f8b2cfe34878b5d3a18
w1y0n0 <wiyono30@gmail.com>
2026-06-04 10:21:04 +0700

    Menampilkan Component Slider dan Category di Halaman Homepage
    - View/Component Homepage
    - Uji Coba Halaman Homepage

--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -1,11 +1,38 @@
 <template>
-    <div class="pb-20 pt-20 text-center">
-        HALAMAN HOMEPAGE
+    <div class="pb-20 pt-20">
+        <div class="container mx-auto grid grid-cols-1 p-3 sm:w-full md:w-5/12">
+
+            <!-- slider -->
+            <div class="grid grid-cols-1 bg-white rounded shadow-md p-1 text-sm">
+                <Slider />
+            </div>
+
+            <!-- categoryHome -->
+            <CategoryHome />
+
+            <!-- campaign -->
+
+        </div>
+
     </div>
 </template>
 
 <script>
+
+//component slider
+import Slider from '../../components/Slider.vue'
+
+//component categoryHome
+import CategoryHome from '../../components/CategoryHome.vue'
+
 export default {
+
     name: 'HomeComponent',
+
+    components: {
+        Slider,         // <-- register component slider
+        CategoryHome,   // <-- register component CategoryHome
+    },
+
 }
 </script>
