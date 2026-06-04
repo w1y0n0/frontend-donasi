b7fdd9cdc7fa22064547374762012eb354aaaec8
w1y0n0 <wiyono30@gmail.com>
2026-06-04 10:14:08 +0700

    Membuat Component Category Home
    - Membuat Component Category Home
    - Edit Module Category Vuex

--- src/store/module/category.js
+++ src/store/module/category.js
@@ -1,3 +1,6 @@
+//import global API
+import Api from '../../api/Api'
+
 const category = {
 
     //set namespace true
@@ -6,15 +9,41 @@ const category = {
     //state
     state: {
 
+        //index categories
+        categories: [],
+
     },
 
     //mutations
     mutations: {
 
+        //set state categories dengan data dari response 
+        SET_CATEGORIES(state, data) {
+            state.categories = data
+        },
+
     },
 
     //actions
     actions: {
+
+        //action getCategoryHome
+        getCategoryHome({ commit }) {
+
+            //get data sliders ke server
+            Api.get('/categoryHome')
+                .then(response => {
+
+                    //commit ke mutation SET_CATEGORIES dengan response data
+                    commit('SET_CATEGORIES', response.data.data)
+
+                }).catch(error => {
+
+                    //show error log dari response
+                    console.log(error)
+
+                })
+        },
 
     },
 
