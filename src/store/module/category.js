365523c5df8a1332ad88c976b75d800ac86a0e05
w1y0n0 <wiyono30@gmail.com>
2026-06-09 07:35:42 +0700

    Menampilkan Detail Category
    - View/Component Detail Category
    - Edit Module Category Vuex
    - Uji Coba Detail Category

--- src/store/module/category.js
+++ src/store/module/category.js
@@ -12,6 +12,12 @@ const category = {
         //index categories
         categories: [],
 
+        //detail category
+        category: {},
+
+        //campaign category
+        campaignCategory: []
+
     },
 
     //mutations
@@ -22,6 +28,16 @@ const category = {
             state.categories = data
         },
 
+        //set state category dengan data dari response 
+        DETAIL_CATEGORY(state, data) {
+            state.category = data
+        },
+
+        //set state campaignCategory dengan data dari response 
+        CAMPAIGN_CATEGORY(state, data) {
+            state.campaignCategory = data
+        },
+
     },
 
     //actions
@@ -30,7 +46,7 @@ const category = {
         //action getCategoryHome
         getCategoryHome({ commit }) {
 
-            //get data sliders ke server
+            //get data category Home ke server
             Api.get('/categoryHome')
                 .then(response => {
 
@@ -48,12 +64,33 @@ const category = {
         //action getCategory
         getCategory({ commit }) {
 
-            //get data sliders ke server
+            //get data category ke server
             Api.get('/category')
                 .then(response => {
 
                     //commit ke mutation SET_CATEGORIES dengan response data
                     commit('SET_CATEGORIES', response.data.data.data)
+
+                }).catch(error => {
+
+                    //show error log dari response
+                    console.log(error)
+
+                })
+        },
+
+        //action getCategory
+        getDetailCategory({ commit }, slug) {
+
+            //get data detail category ke server
+            Api.get(`/category/${slug}`)
+                .then(response => {
+
+                    //commit ke mutation DETAIL_CATEGORY dengan response data
+                    commit('DETAIL_CATEGORY', response.data.data)
+
+                    //commit ke mutation CAMPAIGN_CATEGORY dengan response data
+                    commit('CAMPAIGN_CATEGORY', response.data.data.campaigns)
 
                 }).catch(error => {
 
